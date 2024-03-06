a5abfda5eec9b80ba585cd52619317c1411010a8
Utsav Roy <utsavroy44@gmail.com>
2024-03-06 20:27:06 +0530

    Base Code

--- src/main/java/io/getarrays/server/ServerApplication.java
+++ src/main/java/io/getarrays/server/ServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServerApplication {
 
+	//Base code
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
